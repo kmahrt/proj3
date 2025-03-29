dd5dd00f8a661a045307b81030492b6da0af3248
Kat <cookie@mahrt.com>
2025-03-28 22:44:56 -0600

    Updated for Project 4 // new dark theme button

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ function showSection(sectionId) {
     });
     document.getElementById(sectionId).classList.add("active");
   }
+
+
+function toggleTheme() {
+  let themeLink = document.getElementById('theme-style');
+  
+  if (!themeLink) {
+      themeLink = document.createElement('link');
+      themeLink.rel = 'stylesheet';
+      themeLink.id = 'theme-style';
+      themeLink.href = 'css/dark-theme.css';
+      document.head.appendChild(themeLink);
+  } else {
+      themeLink.remove();
+  }
+}
